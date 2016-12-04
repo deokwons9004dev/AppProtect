eae05d31bcc62d0a95ad308d36c831b1c2d6e338
Ryan Sickles <rsickles@andrew.cmu.edu>
2016-12-04 15:16:06 -0500

    Modal view legal terms added

--- web/js/main.js
+++ web/js/main.js
@@ -10,4 +10,18 @@ $(document).ready(function() {
   			$("#search_block").hide();
   		}
 		});
+
+		$('.modal').modal({
+      dismissible: false, // Modal can be dismissed by clicking outside of the modal
+      opacity: .5, // Opacity of modal background
+      in_duration: 300, // Transition in duration
+      out_duration: 200, // Transition out duration
+      starting_top: '4%', // Starting top style attribute
+      ending_top: '10%', // Ending top style attribute
+      ready: function(modal, trigger) { // Callback for Modal open. Modal and trigger parameters available.
+        console.log(modal, trigger);
+      },
+      complete: function() { console.log('Closed'); } // Callback for Modal close
+    }
+  );
   });
