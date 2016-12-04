2fc97b0759086c181483796ce169dcdc339c31fc
Ryan Sickles <rsickles@andrew.cmu.edu>
2016-12-04 03:04:38 -0500

    Added update of CSS

--- web/js/main.js
+++ web/js/main.js
@@ -2,6 +2,12 @@ $(document).ready(function() {
     $('select').material_select();
     $( '#type_test' ).change(function() {
     	var x = $( "select option:selected" ).text();
-  		console.log(x);
+    	if(x=="Website"){
+    		$("#search_block").show();
+    		$("#testing_inputs").hide();
+  		}else{
+  			$("#testing_inputs").show();
+  			$("#search_block").hide();
+  		}
 		});
   });
