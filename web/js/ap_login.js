e23c18a673d96dda67e739283641b5c092c19efb
Ryan Sickles <rsickles@andrew.cmu.edu>
2016-12-04 18:10:20 -0500

    Added login features

--- web/js/ap_login.js
+++ web/js/ap_login.js
@@ -1,21 +1,21 @@
 /* This Javascript is for testing login features. */
 
-/* Socket.io Events */    
+/* Socket.io Events */
 socket.on('login_reject', function (error) {
-	alert('Login Failed.\n' + error); 
+	alert('Login Failed.\n' + error);
 });
 socket.on('login_success', function (user) {
-	alert('Login Success.\nUser:' + user); 
+	alert('Login Success.\nUser:' + user);
 	$('#tokenButton').css('display', 'block');
 	$('#ver_site').css('display', 'block');
 	$('#verifyButton').css('display', 'block');
 	getVerifiedSites();
 });
 
-/* Button Handlers */    
-$('#loginButton').click(function () {
+/* Button Handlers */
+$('#agree_terms_button').click(function () {
 	socket.emit('login', {
-		id: $('#id_login').val(),
-		ps: $('#ps_login').val(),
+		id: $('#username').val(),
+		ps: $('#password').val(),
 	});
 });
